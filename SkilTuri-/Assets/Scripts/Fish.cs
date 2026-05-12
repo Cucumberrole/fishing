dcd772b0c8f7b522c11ef7f7161ae69c6df02228
神 悠槙 <jin.0105.otoko@gmail.com>
2026-05-12 19:04:22 +0900

    魚が動くようになりました

---
+++ SkilTuri-/Assets/Scripts/Fish.cs
@@ -0,0 +1,46 @@
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public float speed = 2f;
+    public float moveDistance = 3f;
+
+    private Vector3 startPos;
+    private bool movingRight = true;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (movingRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+            if (transform.position.x > startPos.x + moveDistance)
+            {
+                movingRight = false;
+                Flip();
+            }
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            if (transform.position.x < startPos.x - moveDistance)
+            {
+                movingRight = true;
+                Flip();
+            }
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
