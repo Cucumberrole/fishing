05f9b24d196bb5045dc3f755ac84a3612e9f3980
a <sdt2595sc0024@edu.sca.ac.jp>
2026-05-27 16:46:55 +0900

    GameManegerを追加しました

---
+++ SkilTuri-/Assets/Scripts/GManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GManager : MonoBehaviour
+{
+    public static GManager instance = null;
+    public int money;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
