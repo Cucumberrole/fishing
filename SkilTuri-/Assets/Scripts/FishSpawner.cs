e822f06b06a807dd1bfffa304e53dd172ce233fc
神悠槙 <jin.0105.otoko@gmail.com>
2026-06-26 16:47:58 +0900

    メインゲームに桟橋の設定を追加しました。多分正常に動くはず

--- SkilTuri-/Assets/Scripts/FishSpawner.cs
+++ SkilTuri-/Assets/Scripts/FishSpawner.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class FishSpawner : MonoBehaviour
 {
     public GameObject fishPrefab;
+
+    // GManagerが存在しなかった場合に使う初期値
     public int spawnCount = 20;
 
     public Vector2 spawnMin;
@@ -10,13 +12,42 @@ public class FishSpawner : MonoBehaviour
 
     void Start()
     {
+        // 強化済みのスポーン数を受け取る
+        if (GManager.instance != null)
+        {
+            spawnCount = GManager.instance.spawnCount;
+        }
+        else
+        {
+            Debug.LogWarning("GManagerが見つかりません");
+        }
+
         for (int i = 0; i < spawnCount; i++)
         {
             Vector2 pos = new Vector2(Random.Range(spawnMin.x, spawnMax.x), Random.Range(spawnMin.y, spawnMax.y));
 
             GameObject fishObj = Instantiate(fishPrefab, pos, Quaternion.identity);
+
             Fish fish = fishObj.GetComponent<Fish>();
-            fish.size = (FishSize)Random.Range(0, 3);
+
+            if (fish == null)
+            {
+                Debug.LogError(fishPrefab.name + "にFishが付いていません");
+
+                Destroy(fishObj);
+                continue;
+            }
+
+            if (GManager.instance != null)
+            {
+                // 桟橋レベルに応じた確率でサイズを決める
+                fish.size = GManager.instance.GetRandomFishSize();
+            }
+            else
+            {
+                // GManagerがない場合は均等抽選
+                fish.size = (FishSize)Random.Range(0, 3);
+            }
         }
     }
 }
