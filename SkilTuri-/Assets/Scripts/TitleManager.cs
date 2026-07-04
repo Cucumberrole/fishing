5d36d3ff31253123de710308a70ec1833d8007dc
神 悠槙 <jin.0105.otoko@gmail.com>
2026-07-04 13:35:53 +0900

    音と、Title変更

--- SkilTuri-/Assets/Scripts/TitleManager.cs
+++ SkilTuri-/Assets/Scripts/TitleManager.cs
@@ -3,11 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    private void Start()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlayGameBGM2();
+        }
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (AudioManager.Instance != null) AudioManager.Instance.PlayButtonSE();
+            if (AudioManager.Instance != null)
+            {
+                AudioManager.Instance.PlayButtonSE();
+            }
+
             SceneManager.LoadScene("MainGame");
         }
     }
